256a84efa9c2649daa30c79bf97584ec0251c5a7
Peter Sedlacek <kolac12345@gmail.com>
2024-06-05 14:09:25 +0200

    skoro vsetko ide okrem nastavenia hodnoty PWM

--- POIT_zadanie/POIT_zadanie.ino
+++ POIT_zadanie/POIT_zadanie.ino
@@ -1,21 +1,37 @@
 int sensorValue;
-int sensorLow = 380;
-int sensorHigh = 620;
-
+int sensorLow = 0;
+int sensorHigh = 500;
+bool manual = false;
 void setup() {
   Serial.begin(9600);
-  pinMode(9, OUTPUT);
-  pinMode(A0, INPUT);// Nastavíme vstavanú LED ako výstup
+  pinMode(9, OUTPUT);// Nastavíme vstavanú LED ako výstup
+  pinMode(A5, INPUT);
 }
 
 void loop() {
-  sensorValue = analogRead(A0);
-  Serial.println(sensorValue);
+  if (Serial.available() > 0) {
+    char receivedChar = Serial.read();
+    if(receivedChar == '1'){
+      manual = true;
+      }
+    else{
+      manual = false;
+      }
+  }
+  if(manual){
+    
+    }
+  else{
+    sensorValue = analogRead(A5);
+    
   
-  // Inverzne mapovanie hodnôt zo senzora na rozsah PWM (255-0)
-  int brightness = map(sensorValue, sensorLow, sensorHigh, 255, 0);
-  // Nastavíme intenzitu svietenia vstavanej LED
-  analogWrite(9, brightness);
+    // Inverzne mapovanie hodnôt zo senzora na rozsah PWM (255-0)
+    int brightness = map(sensorValue, sensorLow, sensorHigh, 255, 0);
+    // Nastavíme intenzitu svietenia vstavanej LED
+    Serial.println(sensorValue);
+    analogWrite(9, 255-brightness);
+  }
   
-  delay(10);
+  
+  delay(2000);
 }
--- app.py
+++ app.py
@@ -5,58 +5,108 @@ import time
 import random
 import math
 import serial
-async_mode = None
+import MySQLdb 
+import configparser as ConfigParser
+import json
 
+async_mode = None
 app = Flask(__name__)
 app.config['DEBUG'] = True
-
 app.config['SECRET_KEY'] = 'secret!'
 socketio = SocketIO(app, async_mode=async_mode)
 thread = None
-thread_lock = Lock() 
+thread_lock = Lock()
+
+config = ConfigParser.ConfigParser()
+config.read('config.cfg')
+myhost = config.get('mysqlDB', 'host')
+myuser = config.get('mysqlDB', 'user')
+mypasswd = config.get('mysqlDB', 'passwd')
+mydb = config.get('mysqlDB', 'db')
+
 ser = serial.Serial('/dev/ttyACM0', 9600)
+ser.baudrate = 9600
+ser.reset_input_buffer()
 
 def background_thread(args):
     count = 0       
-    dataList = []     
+    dataList = []
+    db = MySQLdb.connect(host=myhost,user=myuser,passwd=mypasswd,db=mydb)
     while True:
+        print(args)
+        if args:
+          dbV = args.get('db_value')
+        else:
+          dbV = 'stop'  
         if ser.in_waiting > 0:
             line = ser.readline().decode('utf-8').rstrip()
+        if dbV == 'start':
+          dataDict = {
+            "x": count,
+            "y": int(line)
+            }
+          dataList.append(dataDict)
+        else:
+            print(str(dataList))
+            if len(dataList)>0:
+                fuj = str(dataList).replace("'", "\"")
+                dataJsonString = json.dumps(dataList, ensure_ascii=False)
+                print(dataJsonString)
+                fo = open("static/files/test.txt","a+")    
+                fo.write("%s\r\n" %dataJsonString)
+                cursor = db.cursor()
+                cursor.execute("SELECT MAX(id) FROM zadanie")
+                maxid = cursor.fetchone()
+                cursor.execute("INSERT INTO zadanie (id,hodnoty) VALUES (%s,%s)", (maxid[0]+1,fuj))
+                db.commit()
+                dataList = []
+                count = 0
+            print(str(dataList))
         time.sleep(2)    
         socketio.emit('my_response',
                       {'data': line, 'count': count},
                       namespace='/test')  
         count+=1
-@app.route('/')
+    db.close()
+@app.route('/', methods=['GET', 'POST'])
 def hello():
-    return render_template('index.html')
+    return render_template('tabs.html')
 
-@socketio.on('my_event', namespace='/test')
-def test_message(message):   
-#    session['receive_count'] = session.get('receive_count', 0) + 1 
-    session['A'] = message['value']    
-#    emit('my_response',
-#         {'data': message['value'], 'count': session['receive_count']})
- 
+@app.route('/read/<string:num>', methods=['GET', 'POST'])
+def readmyfile(num):
+    fo = open("static/files/test.txt","r")
+    rows = fo.readlines()
+    return rows[int(num)-1]
+
+@app.route('/dbdata/<string:num>', methods=['GET', 'POST'])
+def dbdata(num):
+  db = MySQLdb.connect(host=myhost,user=myuser,passwd=mypasswd,db=mydb)
+  cursor = db.cursor()
+  print(num)
+  cursor.execute("SELECT hodnoty FROM  zadanie WHERE id=%s", num)
+  rv = cursor.fetchone()
+  return str(rv[0])
+
 @socketio.on('disconnect_request', namespace='/test')
 def disconnect_request():
-    session['receive_count'] = session.get('receive_count', 0) + 1
     emit('my_response',
          {'data': 'Disconnected!', 'count': session['receive_count']})
     disconnect()
 
 @socketio.on('connect', namespace='/test')
-def test_connect():
+def connect():
     global thread
     with thread_lock:
         if thread is None:
-            thread = socketio.start_background_task(target=background_thread, args=session._get_current_object())
-#    emit('my_response', {'data': 'Connected', 'count': 0})
-   
+            thread = socketio.start_background_task(target=background_thread,args=session._get_current_object())
+
+@socketio.on('db_event', namespace='/test')
+def db_message(message):
+    session['db_value'] = message['value']
 
 @socketio.on('disconnect', namespace='/test')
 def test_disconnect():
     print('Client disconnected', request.sid)
-
+    
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5000, debug=True)
